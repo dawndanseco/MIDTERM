880cb6a7b5e069ad2503b52561cccc28b5080d28
dawn <qdabdanseco@tip.edu.ph>
2023-11-08 16:10:50 +0800

    Files Updated

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react';
 import { View, Text, TextInput, Pressable, StyleSheet, ImageBackground, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -56,24 +57,41 @@ const Homescreen = () => {
                     </View>
                 </View>
                 <View style={styles.lineSeparator}></View>
+                <View style={styles.bottomButtonsContainer}>
+                    <Pressable
+                        style={({ pressed }) => [
+                            {
+                                backgroundColor: pressed ? 'darkblue' : '#3B4B69',
+                            },
+                            styles.circularButton,
+                        ]}
+                        onPress={() => {
+                            // Handle Home button press
+                        }}
+                    >
+                        <Svg width="40" height="40" viewBox="0 0 40 40">
+                            <Circle cx="20" cy="20" r="19" stroke="white" strokeWidth="2" fill="#3B4B69" />
+                            <Path d="M18 12V28M12 18H28" stroke="white" strokeWidth="2" />
+                        </Svg>
+                    </Pressable>
 
-                {/* Circular Pressable button with a plus sign */}
-                <Pressable
-                    style={({ pressed }) => [
-                        {
-                            backgroundColor: pressed ? 'darkblue' : '#3B4B69',
-                        },
-                        styles.circularButton,
-                    ]}
-                    onPress={() => {
-                        // Handle Add button press
-                    }}
-                >
-                    <Svg width="40" height="40" viewBox="0 0 40 40">
-                        <Circle cx="20" cy="20" r="19" stroke="white" strokeWidth="2" fill="#3B4B69" />
-                        <Path d="M18 12V28M12 18H28" stroke="white" strokeWidth="2" />
-                    </Svg>
-                </Pressable>
+                    <Pressable
+                        style={({ pressed }) => [
+                            {
+                                backgroundColor: pressed ? 'darkblue' : '#3B4B69',
+                            },
+                            styles.circularButton,
+                        ]}
+                        onPress={() => {
+                            // Handle Add button press
+                        }}
+                    >
+                        <Svg width="60" height="70" viewBox="0 0 40 40">
+                            <Circle cx="20" cy="20" r="19" stroke="white" strokeWidth="2" fill="#3B4B69" />
+                            <Path d="M18 12V28M12 18H28" stroke="white" strokeWidth="2" />
+                        </Svg>
+                    </Pressable>
+                </View>
             </View>
             <ImageBackground
                 source={require('.././assets/welcomereminder.png')}
@@ -232,6 +250,13 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 2,
         elevation: 5,
+    },
+    bottomButtonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+        top: 330,
+        right: 95,
     },
 });
 
